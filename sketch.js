d91e62d1cc05829afa1fffc110a185b967353f4e
MDRayaan <75481602+MDRayaan@users.noreply.github.com>
2020-12-21 12:42:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,15 +17,15 @@ car.velocityX=0;
 var deformation=0.5*weight*speed*speed/22509;
 
 if(deformation>180){
-  car.shapeColor=color(255,0,0);
+  car.shapeColor="yellow";
 }
 
 if(deformation>180 && deformation>100){
-  car.shapeColor=color(230,230,0);
+  car.shapeColor="red";
 }
 
 if(deformation<100){
-  car.shapeColor=color(0,255,0);
+  car.shapeColor="green";
 }
 
 }
